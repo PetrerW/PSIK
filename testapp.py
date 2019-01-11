1fbda38ce8417d25ea1a451710f1294382574138
PetrerW <piotrw76@gmail.com>
2019-01-11 14:24:54 +0100

    Add msg reason

--- testapp.py
+++ testapp.py
@@ -37,7 +37,8 @@ class L2Switch(app_manager.RyuApp):
         # Datapath ID of the switch that has sent the message
         packet_id = msg.buffer_id
         switch_id = msg.datapath.id
-        print("I got a packetIn message (", packet_id, ") from switch: ", switch_id, ". Message: \"", msg, "\"")
+        reason = msg.reason
+        print("I got a packetIn message (", packet_id, ") from switch: ", switch_id, ". Reason: ", reason, ", Message: \"", msg, "\"")
 
         # Input port of the packet
         in_port = msg.in_port
