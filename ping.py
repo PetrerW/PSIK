90facda469634415c8ac3231dd91b85204bb5e7e
PetrerW <piotrw76@gmail.com>
2019-01-13 16:40:33 +0100

    Edit counters

--- ping.py
+++ ping.py
@@ -40,8 +40,10 @@ class IcmpResponder(app_manager.RyuApp):
         super(IcmpResponder, self).__init__(*args, **kwargs)
         #Wheter started offloading
         self._init = True
-        #How many packets passed through a switch
-        self._counter = 0
+        #How many packets passed through s1
+        self._counter_s3 = 0
+        #How many packets passed through s2
+        self._counter_s4 = 0
         #Wheter offloading or not
         self._offload = False
 
@@ -101,21 +103,40 @@ class IcmpResponder(app_manager.RyuApp):
                 # Add flows with src_mac addresses and sending to the controller
                 self.add_mac_src_flow(dp, switch_id, src_mac, msg.buffer_id, output_port)
         else:
-            self._counter = self._counter + 1
-            print("Counter = ", self._counter)
-            if self._counter >= 5:
-                self._counter = 0
-                
-                output_port = self.choose_output_port(src_mac, switch_id, self._offload)
-                self.modify_flow(dp, 1, '00:00:00:00:00:01', msg.buffer_id, output_port)
-                self.modify_flow(dp, 2, '00:00:00:00:00:02', msg.buffer_id, output_port)
-                
-                #Change offloading state
-                if self._offload == True:
-                    self._offload = False
-                else:
-                    self._offload = True
-                print("offload = ", self._offload)
+            if switch_id == 3:
+                self._counter_s3 = self._counter_s3 + 1
+                print("Counter_s", switch_id, " = ", self._counter_s3)
+                if self._counter_s3 >= 5:
+                    self._counter_s3 = 0
+                    
+                    output_port = self.choose_output_port(src_mac, switch_id, self._offload)
+                    self.modify_flow(dp, 1, '00:00:00:00:00:01', msg.buffer_id, output_port)
+                    self.modify_flow(dp, 2, '00:00:00:00:00:02', msg.buffer_id, output_port)
+                    
+                    #Change offloading state
+                    if self._offload == True:
+                        self._offload = False
+                    else:
+                        self._offload = True
+                    print("offload = ", self._offload)
+            if switch_id == 4:
+                self._counter_s4 = self._counter_s4 + 1
+                print("Counter_s", switch_id, " = ", self._counter_s4)
+                if self._counter_s4 >= 5:
+                    self._counter_s4 = 0
+                    
+                    output_port = self.choose_output_port(src_mac, switch_id, self._offload)
+                    self.modify_flow(dp, 1, '00:00:00:00:00:01', msg.buffer_id, output_port)
+                    self.modify_flow(dp, 2, '00:00:00:00:00:02', msg.buffer_id, output_port)
+                    
+                    #Change offloading state
+                    if self._offload == True:
+                        self._offload = False
+                    else:
+                        self._offload = True
+                    print("offload = ", self._offload)
+
+
 
     def remove_controller_flow(self, dp, switch_id):
         ofp = dp.ofproto
